c98ac776bf4d0bc6be0c15d7c608a0eb90f6fbb0
Jakub Kaczmarczyk <jakubkaczmarczyk103@gmail.com>
2024-01-06 13:04:27 +0100

    remeber login state

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { CommonModule } from '@angular/common';
@@ -12,11 +12,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
   model: any = {}
   loggedIn = false;
 
   constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+    this.getCurrentuser();
+  }
+
+  getCurrentuser() {
+    this.accountService.currentUser$.subscribe({
+      next: user => this.loggedIn = !!user,
+      error: error => console.log(error)
+    })
+  }
 
   login() {
     this.accountService.login(this.model).subscribe({
@@ -29,6 +40,7 @@ export class NavComponent {
   }
 
   logout() {
+    this.accountService.logout();
     this.loggedIn = false;
   }
 }
