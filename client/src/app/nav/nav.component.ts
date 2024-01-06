103b7963d96c95a05c95ebe70f7350d5db5eaff6
Jakub Kaczmarczyk <jakubkaczmarczyk103@gmail.com>
2024-01-06 11:31:52 +0100

    navbar added

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [
+    CommonModule, 
+    FormsModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+  model: any = {}
+  loggedIn = false;
+
+  constructor(private accountService: AccountService) { }
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+}
