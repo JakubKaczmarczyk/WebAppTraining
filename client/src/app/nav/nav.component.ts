7db3b79684580afba320eb7f75587300d0b3492a
Jakub Kaczmarczyk <jakubkaczmarczyk103@gmail.com>
2024-01-08 18:58:30 +0100

    end of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -4,34 +4,36 @@ import { AccountService } from '../_services/account.service';
 import { CommonModule } from '@angular/common';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router, RouterModule } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [
     CommonModule, 
-    FormsModule],
+    FormsModule,
+    RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toaster: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
-      error: error => console.log(error)
+      next: () => this.router.navigateByUrl('/members'),
+      error: error => this.toaster.error(error.error)
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
