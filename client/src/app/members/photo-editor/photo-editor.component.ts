dbce3a364ce8173c95120e729d0f9d70594f9067
Jakub Kaczmarczyk <jakubkaczmarczyk103@gmail.com>
2024-06-01 12:11:05 +0200

    before npm file upload

---
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Member } from '../../_models/member';
+
+@Component({
+  selector: 'app-photo-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './photo-editor.component.html',
+  styleUrl: './photo-editor.component.css'
+})
+export class PhotoEditorComponent implements OnInit {
+  @Input() member: Member | undefined;
+
+  
+
+}
