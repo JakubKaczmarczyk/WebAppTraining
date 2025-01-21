7dfc403836c0bc9eb4f8d8fa480c2d26bd94a1d5
Jakub Kaczmarczyk <jakubkaczmarczyk103@gmail.com>
2025-01-21 21:09:52 +0100

    docker

--- API/Program.cs
+++ API/Program.cs
@@ -6,9 +6,19 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 using System;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure Kestrel to listen on all network interfaces and specific ports
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5000); // HTTP
+    options.ListenAnyIP(5001, listenOptions => listenOptions.UseHttps("/app/https/localhost.pfx")); // HTTPS
+});
+
+// Add services
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
@@ -16,6 +26,7 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+// Middleware
 app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
@@ -26,6 +37,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// Database migration and seeding
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 try
